87e97039042907acc0d3fc0c8437ad21d7d9a4f5
GURLEENK26 <93379248+GURLEENK26@users.noreply.github.com>
2021-11-01 16:26:15 +0530

    Update GAME.py

--- GAME.py
+++ GAME.py
@@ -1,10 +1,10 @@
 import random as ayush
-print("!-------------------------- Rock Paper and scissor -----------------------------!")
+print("!-------------------------- Rock Paper and Scissor -----------------------------!")
 print("Choose one of them \n r for Rock \n p for Paper \n s for Scissor")
-print("Winning Rules of the Rock paper scissor game as follows: \n"
-                                +"Rock vs paper->paper wins \n"
-                                + "Rock vs scissor->Rock wins \n"
-                                +"paper vs scissor->scissor wins \n")
+print("Winning Rules of the Rock Paper Scissor game as follows: \n"
+                                +"Rock vs Paper->Paper wins \n"
+                                + "Rock vs Scissor->Rock wins \n"
+                                +"Paper vs Scissor->Scissor wins \n")
 while True:
     choice = int(input("Your turn: "))
     while choice > 3 or choice < 1:
@@ -12,9 +12,9 @@ while True:
     if choice == 1:
       choice_name = 'Rock'
     elif choice == 2:
-      choice_name = 'paper'
+      choice_name = 'Paper'
     else:
-      choice_name = 'scissor'
+      choice_name = 'Scissor'
     print("user choice is: " + choice_name)
     print("\nNow its computer turn.......")
     comp_choice = ayush.randint(1, 3)
@@ -23,25 +23,25 @@ while True:
     if comp_choice == 1:
       comp_choice_name = 'Rock'
     elif comp_choice == 2:
-      comp_choice_name = 'paper'
+      comp_choice_name = 'Paper'
     else:
-      comp_choice_name = 'scissor'
+      comp_choice_name = 'Scissor'
     print("Computer choice is: " + comp_choice_name)
   
     print(choice_name + " V/s " + comp_choice_name)                        
     if ((choice == 1 and comp_choice == 2) or
         choice == 2 and comp_choice == 1):
-      print("paper wins => ", end = "")
-      result="paper"
+      print("Paper wins => ", end = "")
+      result="Paper"
 
     elif ((choice == 3 and comp_choice == 1) or
           choice == 1 and comp_choice == 3):
-      print("scissor wins => ", end = "")
-      result="scissor"
+      print("Rock wins => ", end = "")
+      result="Rock"
 
     else:
-      print("rock wins => ", end = "")
-      result="rock"
+      print("Scissor wins => ", end = "")
+      result="Scissor"
     if result == choice_name:
       print("<== User wins ==>")
     else:

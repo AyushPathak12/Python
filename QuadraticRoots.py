a1ea6613cecd916c4c9a5d7a9dcd74eb563b4f9f
GURLEENK26 <93379248+GURLEENK26@users.noreply.github.com>
2021-11-02 20:03:28 +0530

    Update QuadraticRoots.py

--- QuadraticRoots.py
+++ QuadraticRoots.py
@@ -5,22 +5,24 @@ while choice=='y' or choice=='Y':
     a=int(input("Enter a:"))
     b=int(input("Enter b:"))
     c=int(input("Enter c:"))
+    delta=b*b-4*a*c
 
     if a==0:
         print("Value of", a, 'should not be zero')
         print("\n Aborting!!!!!!")
     else:
-        delta=b*b-4*a*c
         if delta>0:
             root1=(-b+math.sqrt(delta))/(2*a)
             root2=(-b-math.sqrt(delta))/(2*a)
-            print("Roots are REAL and UNEQUAL")
-            print("Root1=",root1,"Root2=",root2)
+            print("Roots are REAL and UNEQUAL",(root1, root2))
+            
         elif delta==0:
             root1=-b/(2*a)
-            print("Roots are REAL and EQUAL")
-            print("Root1=", root1,"Root2=", root2)
+            root2=-b/(2*a)
+            print("Roots are REAL and EQUAL",(root1, root2))
+            
         else:
-            print("Roots are COMPLEX and IMAGINARY")   
-            choice=input("Press y to continue else press any other key.")
-    
+            root1 = root2 = -b / (2 * a)
+            imaginary = math.sqrt(-delta) / (2 * a)
+            print("Roots are COMPLEX and IMAGINARY",(root1, imaginary, root2, imaginary))   
+        choice=input("Press y to continue else press any other key.")

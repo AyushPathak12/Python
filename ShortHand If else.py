a21c13108a42bd12d5c4863731c408ae6abf2773
Ayush Pathak <60485586+AyushPathak12@users.noreply.github.com>
2021-11-06 11:18:53 +0530

    Update ShortHand If else.py

--- ShortHand If else.py
+++ ShortHand If else.py
@@ -1,6 +1,4 @@
-a=int(input("Enter first number\n"))
-b=int(input("Enter second number\n"))
-if a>b:
-  print(a, "is greater than" ,b)
-else:
-  print(a, "is smaller than" ,b)
+a=int(input("Enter a\n"))
+b=int(input("Enter b\n"))
+
+print("a is greater than b") if a>b else print("a is smaller than b")
